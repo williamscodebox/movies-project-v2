69ceeb55b9cb80fa3f74969c30e0e655772e03e7
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-30 23:20:52 -0400

    updated backend index

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 console.log("Getting up and running...");
+// Packages
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -7,6 +8,9 @@ import path from "path";
 //Files
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+// import genreRoutes from "./routes/genreRoutes.js";
+// import moviesRoutes from "./routes/moviesRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
 
 // Configuration
 dotenv.config();
@@ -23,5 +27,11 @@ const PORT = process.env.PORT || 3000;
 
 // Routes
 app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/genre", genreRoutes);
+// app.use("/api/v1/movies", moviesRoutes);
+// app.use("/api/v1/upload", uploadRoutes);
+
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
