b9ae91bcf7713bdb93f97a042c3a30a77995176c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-30 23:24:24 -0400

    added genreRoutes.js

---
+++ backend/routes/genreRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+// Controllers
+import {
+  createGenre,
+  updateGenre,
+  removeGenre,
+  listGenres,
+  readGenre,
+} from "../controllers/genreController.js";
+
+// Middlewares
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").post(authenticate, authorizeAdmin, createGenre);
+router.route("/:id").put(authenticate, authorizeAdmin, updateGenre);
+router.route("/:id").delete(authenticate, authorizeAdmin, removeGenre);
+router.route("/genres").get(listGenres);
+router.route("/:id").get(readGenre);
+
+export default router;
