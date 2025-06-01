7e44cea0a7387bb2a4242bbcb4fc6b4fefffde0d
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-01 15:09:22 -0400

    updated userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,17 +15,15 @@ import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router
-  .route("/")
-  .post(createUser)
-  .get(authenticate, authorizeAdmin, getAllUsers);
+router.route("/").post(createUser);
+// .get(authenticate, authorizeAdmin, getAllUsers);
 
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
 router
   .route("/profile")
-  .get(authenticate, getCurrentUserProfile)
+  // .get(authenticate, getCurrentUserProfile)
   .put(authenticate, updateCurrentUserProfile);
 
 export default router;
