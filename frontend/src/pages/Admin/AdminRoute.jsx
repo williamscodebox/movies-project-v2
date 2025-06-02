555c0706ad44daecc7daf5ef177eef19e4d9b9e2
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-02 15:25:47 -0400

    updated AdminRoute

--- frontend/src/pages/Admin/AdminRoute.jsx
+++ frontend/src/pages/Admin/AdminRoute.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 const AdminRoute = () => {
-  return <div>AdminRoute</div>;
+  const { userInfo } = useSelector((state) => state.auth);
+  return userInfo && userInfo.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  );
 };
 
 export default AdminRoute;
