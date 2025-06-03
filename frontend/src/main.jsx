48930b3c373a25a80da170fb9bbaee81a0069f2b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-06-03 12:06:45 -0400

    added AdminDashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,6 +22,10 @@ import Profile from "./pages/User/Profile.jsx";
 import AllMovies from "./pages/Movies/AllMovies.jsx";
 import MovieDetails from "./pages/Movies/MovieDetails.jsx";
 import CreateMovie from "./pages/Admin/CreateMovie.jsx";
+import AdminMoviesList from "./pages/Admin/AdminMoviesList.jsx";
+import UpdateMovie from "./pages/Admin/UpdateMovie.jsx";
+import AllComments from "./pages/Admin/AllComments.jsx";
+import AdminDashboard from "./pages/Admin/Dashboard/AdminDashboard.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -40,6 +44,10 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/movies/genre" element={<GenreList />} />
         <Route path="/admin/movies/create" element={<CreateMovie />} />
+        <Route path="/admin/movies-list" element={<AdminMoviesList />} />
+        <Route path="/admin/movies/update/:id" element={<UpdateMovie />} />
+        <Route path="/admin/movies/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/movies/comments" element={<AllComments />} />
       </Route>
     </Route>
   )
